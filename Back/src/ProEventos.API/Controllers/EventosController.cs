432ba58a9840e12a0f4f5ca7dc5382b6c985f0d1
Gustavo Gama Cardozo <gustavogamacardozo@gmail.com>
2021-11-27 15:00:33 -0300

    Seção 4: Angular

---
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class EventosController : ControllerBase
+  {
+    private readonly DataContext _context;
+    public EventosController(DataContext context)
+    {
+      _context = context;
+    }
+
+    [HttpGet]
+    public IEnumerable<Evento> Get()
+    {
+      return _context.Eventos;
+    }
+
+    [HttpGet("{id}")]
+    public Evento GetByID(int id)
+    {
+      return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
+    }
+
+    [HttpPost]
+    public string Post()
+    {
+      return "Exemplo de Post!";
+    }
+
+    [HttpPut("{id}")]
+    public string Put(int id)
+    {
+      return $"Exemplo de Put com ID = {id}";
+    }
+
+    [HttpDelete("{id}")]
+    public string Delete(int id)
+    {
+      return $"Exemplo de Delete com ID = {id}";
+    }
+
+  }
+}
