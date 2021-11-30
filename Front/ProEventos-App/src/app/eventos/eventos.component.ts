3347e95658cfa57d071646ac99ea152ad749e714
Gustavo Gama Cardozo <gustavogamacardozo@gmail.com>
2021-11-30 12:57:46 -0300

    Two-way-DataBinding no filtro de eventos

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-eventos',
@@ -8,7 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any
+  public eventos: any = [];
+
+  public filteredEventos: any = [];
+  private _filterList: string = ''
+
+  public get filterList(): string{
+    return this._filterList;
+  }
+
+  public set filterList(value: string){
+    this._filterList = value;
+    this.filteredEventos = this.filterList ? this.filterEventos(this.filterList) : this.eventos;
+  }
+
+  filterEventos(filterBy: string): any {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: { tema: string; local: string}) => evento.tema.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filterBy) !== -1
+    )
+  }
+
+
+  public widthImg: number = 150;
+  public marginImg: number = 2;
+  public showImg: boolean = false;
 
   constructor(private http: HttpClient) { }
 
@@ -18,7 +44,10 @@ export class EventosComponent implements OnInit {
 
   getEventos(){
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response,
+        this.filteredEventos = this.eventos
+      },
       error => console.log(error)
     )
   }
