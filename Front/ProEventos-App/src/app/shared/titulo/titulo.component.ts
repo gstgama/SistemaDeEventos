ce13124484013493ebb173717e6715e208689d2c
Gustavo Gama Cardozo <gustavogamacardozo@gmail.com>
2021-12-17 12:17:16 -0300

    Modificando component Titulo.

--- Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
+++ Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
@@ -13,5 +13,8 @@ export class TituloComponent implements OnInit {
   }
 
   @Input() titulo: string = '';
+  @Input() iconClass: string = 'fa fa-user';
+  @Input() subtitulo: string = 'Desde 2021';
+  @Input() botaoListar: boolean = false;
 
 }
