4ebdf1072a13061711894aef8313c9338eb624b6
Gustavo Gama Cardozo <gustavogamacardozo@gmail.com>
2022-02-24 09:05:57 -0300

    Dto e Data Annotation

--- Back/src/ProEventos.Application/Dto/EventoDto.cs
+++ Back/src/ProEventos.Application/Dto/EventoDto.cs
@@ -9,14 +9,28 @@ namespace ProEventos.Application.Dto
     public string Local { get; set; }
     public string DataEvento { get; set; }
 
-    [Required(ErrorMessage = "O campo {0} é necessário."),
+    [Display(Name = "tema"),
+    Required(ErrorMessage = "O campo {0} é necessário."),
     StringLength(50, MinimumLength = 3, ErrorMessage = "{0} deve ter entre 3 e 50 caracteres.")]
     public string Tema { get; set; }
+
+    [Display(Name = "quantidade de pessoas"),
+    Required(ErrorMessage = "O campo {0} é necessário"),
+    Range(1, 120000, ErrorMessage = "O campo {0} deve estar entre 1 e 120000")]
     public int QtdPessoas { get; set; }
+
+    [RegularExpression(@".*\.(gif|jpe?g|bmp|png)$",
+                      ErrorMessage = "Não é uma imagem válida. (gif, jpg, jpeg, bmp, png).")]
     public string ImagemURL { get; set; }
+
+    [Display(Name = "telefone"),
+    Phone(ErrorMessage = "O campo {0} está inválido")]
     public string Telefone { get; set; }
 
-    [EmailAddress(ErrorMessage = "Campo {0} inválido.")]
+
+    [Display(Name = "e-mail"),
+    Required(ErrorMessage = "O campo {0} é necessário"),
+    EmailAddress(ErrorMessage = "É campo {0} está inválido.")]
     public string Email { get; set; }
     public IEnumerable<LoteDto> Lotes { get; set; }
     public IEnumerable<RedeSocialDto> RedesSociais { get; set; }
