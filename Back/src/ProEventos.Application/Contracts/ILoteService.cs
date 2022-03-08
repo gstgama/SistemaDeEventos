53ebd4ed1ca6c42657ed903414da6fbee71f074c
Gustavo Gama Cardozo <gustavogamacardozo@gmail.com>
2022-03-08 11:55:21 -0300

    criação LoteService.cs

--- Back/src/ProEventos.Application/Contracts/ILoteService.cs
+++ Back/src/ProEventos.Application/Contracts/ILoteService.cs
@@ -5,9 +5,9 @@ namespace ProEventos.Application.Contracts
 {
   public interface ILoteService
   {
-    Task<LoteDto> SaveLote(int eventoId, LoteDto[] models);
+    Task<LoteDto[]> SaveLotes(int eventoId, LoteDto[] models);
     Task<bool> DeleteLote(int eventoId, int loteId);
     Task<LoteDto[]> GetLotesByEventoIdAsync(int eventoId);
-    Task<LoteDto[]> GetLoteByIdsAsync(int eventoId, int loteId);
+    Task<LoteDto> GetLoteByIdsAsync(int eventoId, int loteId);
   }
 }
